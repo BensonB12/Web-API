344642aa0b032a03362a7735f7dccca611c7a680
BensonB12 <89985506+BensonB12@users.noreply.github.com>
2023-08-30 07:26:40 -0600

    Up to [EnableCors] on Minimal API

--- Web API/Program.cs
+++ Web API/Program.cs
@@ -3,6 +3,21 @@ using System.Runtime.CompilerServices;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(cfg =>
+    {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(name: "AnyOrigin",
+        cfg =>
+        {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+        });
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,12 +41,13 @@ else
 app.UseHttpsRedirection(); //Middle ware
 //Get http://__/weatherforecast then this middleware 'UseHttpsRedirection'  will response / return with "Nope, I need HTTPS, not HTTP
 
+app.UseCors("AnyOrigin");
 app.UseAuthorization();
 
 app.MapGet("/add", (int num1, int num2) => num1 + num2);
 app.MapGet("/greet", (string name) => $"Greetings {name}! Have you heard of the hapalopilus-nidulans partier? He was a fun guy.");
-app.MapGet("/error", () => Results.Problem());
-app.MapGet("/error/test", () => { throw new Exception("test");  });
-app.MapControllers();
+app.MapGet("/error", () => Results.Problem()).RequireCors("AnyOrigin");
+app.MapGet("/error/test", () => { throw new Exception("test");  }).RequireCors("AnyOrigin");
+app.MapControllers().RequireCors("AnyOrigin");
 
 app.Run();
